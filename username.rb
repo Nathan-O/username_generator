9293a8042a89c8316001598286754f7c0348837b
Nathan Ockerman <ockerman.ns@gmail.com>
2015-09-15 16:17:15 -0700

    Here you go Tom

--- username.rb
+++ username.rb
@@ -2,13 +2,36 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  	if (first === "")
+  		return nil
+  	else
+		first = first.gsub(/\W/,"")[0].downcase
+	end
+  	if (last === "")
+  		return nil
+  	else
+	   last1 = last.gsub(/\W/,"").downcase
+	   last2 = last1.gsub(/\d/,"")
+	   return first + last2
+	end
 end
 
-def format_year
-  nil
+def format_year(year)
+	year = year.to_s
+	if (year.length === 4)
+		annual = year[2] + year [3]
+		return annual
+	else
+		return nil
+	end
 end
 
-def build_username
-  nil
+def build_username(first, last, *nums)
+  name = format_name(first, last)
+  annual = format_year(nums[0])
+  username = name + annual
+  
 end
+
+
+# /\W/
